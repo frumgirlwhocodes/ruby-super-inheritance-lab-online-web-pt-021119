857b72ed2125385aca9275dc11bff8fb349f43d3
lieby weintraub <liebahinda@gmail.com>
2019-03-22 03:26:16 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,7 +1,12 @@
 class Student 
   
   def hello 
-    puts "Hey there! I'm so excited to learn stuff."
+     "Hey there! I'm so excited to learn stuff."
+  end 
+  
+  def raise_hand 
+    "pick me!" 
+  end 
   
   
 end 
